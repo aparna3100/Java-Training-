5c29d0e162a337ddbc608cda05d5101f134923eb
user <user@DESKTOP-OQVA7BB>
2023-02-08 13:09:20 +0530

    first commit

---
+++ javaprograms/src/sample/first/HelloWorld.java
@@ -0,0 +1,19 @@
+package sample.first;
+
+public class HelloWorld 
+{
+	public void display()
+	{
+		System.out.println("Helloworld");
+	}
+
+
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		HelloWorld obj = new HelloWorld();
+		obj.display();
+	}
+
+}
